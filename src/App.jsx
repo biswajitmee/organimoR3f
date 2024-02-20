5a73247306f93044f18f0a3739847ba752f0b5d1
biswajitmee <biswajit.mee@gmail.com>
2024-02-20 22:14:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+import ScrollSection from './ScrollSection';
+
+function App() {
+  
+
+    return (
+        <div>
+            <> <ScrollSection/>
+        </>
+
+    </div>
+    );
+}
+
+export default App;
